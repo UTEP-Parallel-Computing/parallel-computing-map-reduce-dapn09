debb514cd543d299e08f868176629a1e61ee0770
dapn09 <dapn09@gmail.com>
2020-02-14 23:26:31 -0700

    code completed, missing report

--- wordCount.py
+++ wordCount.py
@@ -20,16 +20,17 @@ def countInstances(target, text):
     return sum(1 for _ in re.finditer(r'\b%s\b' % re.escape(str(target).lower()), text))
 
 def findCountsParallel(files, words, numThreads=1):
-    files = openFiles(files)
+    texts = []
+
+    for file in files:
+        texts.append(file.read())
+
     wordCounts = pymp.shared.dict()
 
-    #Initialize dictionary
-    for word in words:
-        wordCounts[word] = 0
-
     with pymp.Parallel(numThreads) as p:
-        for fIndex in p.range(0, len(files)):
-            text = files[fIndex].read()
+        for word in words:
+            wordCounts[word] = 0
+        for text in p.iterate(texts):
             for word in words:
                 count = countInstances(word, text)
                 p.lock.acquire()
@@ -40,17 +41,36 @@ def findCountsParallel(files, words, numThreads=1):
 def main():
     words = ["love", "hate", "death", "night", "sleep", "time", "henry", "hamlet", "you", "my", "blood", "poison", "macbeth", "king", "heart", "honest"]
     files = ["shakespeare1.txt", "shakespeare2.txt", "shakespeare3.txt", "shakespeare4.txt", "shakespeare5.txt", "shakespeare6.txt", "shakespeare7.txt", "shakespeare8.txt"]
-    print(findCountsParallel(files, words, numThreads=4))
 
+    print("Count totals...")
+    print("\nWord\tAppearances\n")
+
+    #Opening files
+    startTime = time.time()
+    docs = openFiles(files)
+    openFilesTime = time.time() - startTime
+
+    startTime = time.time()
+    totals = findCountsParallel(docs, words, numThreads=4)
+    countingTime = time.time() - startTime
+
+    for word, count in totals.items():
+        print(f"{word}\t{count}")
+
+    print("\nTesting with more cores...\n")
     for i in range(0, 10, 2):
         results = 0
         startTime = time.time()
         if i == 0:
-            results = findCountsParallel(files, words, numThreads=1)
-            print(f"Single thread execution time: {time.time() - startTime}")
+            results = findCountsParallel(docs, words, numThreads=1)
+            print(f"1 thread execution time: {time.time() - startTime}")
         else:
-            results = findCountsParallel(files, words, numThreads=i)
+            results = findCountsParallel(docs, words, numThreads=i)
             print(f"{i} threads execution time: {time.time() - startTime}")
+
+    print("\nDisplaying runtimes...\n")
+    print(f"Time spent opening files: {openFilesTime}")
+    print(f"Time spent counting words: {countingTime}\n")
 
 if __name__ == "__main__":
     main()
